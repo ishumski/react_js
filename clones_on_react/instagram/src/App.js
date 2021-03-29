59a2c430479aac28b422631b8964e4867152a5e7
ishumski <whalterwhite23@gmail.com>
2021-03-29 12:01:51 +0300

    Add firebase

--- clones_on_react/instagram/src/App.js
+++ clones_on_react/instagram/src/App.js
@@ -1,15 +1,42 @@
+import React, { useState } from "react";
 import './App.css';
 import Post from './components/Post/Post.js';
+import firebase from "firebase";
 
 
 function App() {
+  
+function componentDidMount(){
+  const db = firebase.database();
+  console.log(db)
+}
+componentDidMount()
+
+
+  const [posts, setPost] = useState([
+    {
+      username: "Ilia",
+      caption: "BLAAAAAA",
+      imageUrl: "https://allthatsinteresting.com/wordpress/wp-content/uploads/2013/09/interesting-pictures-asperatus-clouds.jpg",
+    }
+  ]);
   return (
     <div className="App">
       <div className="app__header">
         <img className="app__hederImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
       </div>
-      <Post />
-     
+
+      {
+        posts.map(post => (
+          <Post
+            username={post.username}
+            caption={post.caption}
+            imageUrl={post.imageUrl}
+          />
+        ))
+      }
+
+
     </div>
   );
 }
--- clones_on_react/instagram/src/components/Post/Post.js
+++ clones_on_react/instagram/src/components/Post/Post.js
@@ -1,9 +1,8 @@
 import style from "./Post.module.css"
 import Avatar from "@material-ui/core/Avatar";
 
-
-function Post() {
-    const username = "Ilia Shumski";
+function Post({ username, caption, imageUrl }) {
+    
     return (
         <div className={style.post}>
             <div className={style.post__header}>
@@ -12,11 +11,11 @@ function Post() {
                     alt={username}
                     src="/static/images/avatar/1.jpg"
                 />
-                <h3 className={style.post__username}>Username</h3>
+                <h3 className={style.post__username}>{username}</h3>
             </div>
 
-            <img className={style.post__image} src="https://allthatsinteresting.com/wordpress/wp-content/uploads/2013/09/interesting-pictures-asperatus-clouds.jpg" alt="" />
-            <h4 className={style.post__text}><strong>Username</strong> : bla bla bla</h4>
+            <img className={style.post__image} src={imageUrl} alt="" />
+            <h4 className={style.post__text}><strong>{username}</strong>: {caption}</h4>
         </div>
     )
 }
