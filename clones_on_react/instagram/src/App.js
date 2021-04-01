e0c526ab8b698b869f75d7eeb688453bb560de6f
ishumski <whalterwhite23@gmail.com>
2021-04-01 15:35:21 +0300

    Add dinamic posts

--- clones_on_react/instagram/src/App.js
+++ clones_on_react/instagram/src/App.js
@@ -2,11 +2,12 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Post from './components/Post/Post.js';
-import { auth, db } from "./firebase.js";
+import { db, auth } from "./firebase.js";
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from "@material-ui/core";
 import ImageUpload from "./ImageUpload.js";
+import InstagramEmbed from "react-instagram-embed";
 
 function getModalStyle() {
   const top = 50;
@@ -35,7 +36,7 @@ function App() {
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
 
-  const [posts, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const [open, setOpen] = useState(false);
   const [openSignIn, setOpenSignIn] = useState(false);
@@ -46,34 +47,30 @@ function App() {
 
   const [user, setUser] = useState(null);
 
-
-
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
-        //пользователь залогинился
-        console.log(authUser);
         setUser(authUser);
       } else {
-        //пользователь разлогинился
         setUser(null);
       }
-    })
+    });
     return () => {
       unsubscribe();
-    }
-  }, [user, username])
-
+    };
+  }, [user, username]);
 
   useEffect(() => {
-    db.collection("posts").onSnapshot(snapshot => {
-      setPost(snapshot.docs.map(doc => (
-        {
-          id: doc.id,
-          post: doc.data()
-        })
-      ));
-    })
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            posts: doc.data(),
+          }))
+        );
+      });
   }, []);
 
   /*
@@ -87,32 +84,27 @@ function App() {
   const signUp = (event) => {
     event.preventDefault();
 
-    auth.createUserWithEmailAndPassword(email, password).then((authUser) => {
-      return authUser.user.updateProfile({
-        displayName: username,
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
       })
-    }).catch((error) => alert(error.message));
-
-    setOpen(false);
-  }
+      .catch((error) => error.message);
+  };
 
   const signIn = (event) => {
     event.preventDefault();
-
-    auth.createUserWithEmailAndPassword(email, password).catch((error) => alert(error.message))
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
 
     setOpenSignIn(false);
-  }
+  };
 
   return (
     <div className="App">
-
-      { user?.displayName ? (
-        <ImageUpload username={user.displayName} />
-      ) : (
-        <h3>Sorry you need to login to upload</h3>
-      )}
-
 
       <Modal
         open={openSignIn}
@@ -120,9 +112,9 @@ function App() {
 
         <div style={modalStyle} className={classes.paper}>
 
-          <form className="app_signUp">
+          <form className="app_signIn">
             <center>
-              <img className="app__signUpImg"
+              <img className="app__signInImg"
                 src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
                 alt="" />
             </center>
@@ -187,33 +179,50 @@ function App() {
       </Modal>
 
       <div className="app__header">
-        <img className="app__hederImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+        <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+        {user ? (
+          <Button onClick={() => auth.signOut()}>Logout</Button>
+        ) : (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )
+        }
       </div>
 
-      {user ?
-        (<Button onClick={() => auth.signOut()}>Logout</Button>) :
-        (<div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {posts.map(({ id, posts }) => (
+            <Post key={id} username={posts.username} caption={posts.caption} imageUrl={posts.imageUrl} />
+          ))}
+
         </div>
-        )
-
-      }
-
-
-
-      {
-        posts.map(({ id, post }) => (
-          <Post
-            key={id}
-            username={post.username}
-            caption={post.caption}
-            imageUrl={post.imageUrl}
+        <div className="app__postsRight">
+          <InstagramEmbed
+            url="https://www.instagram.com/p/CEm4k16Mpsm/"
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName="div"
+            protocol=""
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
           />
-        ))
-      }
+        </div>
+
+      </div>
 
 
+
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ) : (
+        <h3>Sorry you need to login.</h3>
+      )}
     </div>
   );
 }
--- clones_on_react/instagram/src/ImageUpload.js
+++ clones_on_react/instagram/src/ImageUpload.js
@@ -1,19 +1,20 @@
-import { Button } from "@material-ui/core";
+
 import React, { useState } from "react";
-import { storage, db } from "./firebase.js";
+import { Button } from "@material-ui/core";
 import firebase from "firebase";
+import { storage, db } from "./firebase.js";
+import './ImageUpload.css';
 
 function ImageUpload({ username }) {
+    const [caption, setCaption] = useState("");
+    const [image, setImage] = useState(null);
+    const [progress, setProgress] = useState(0);
 
-    const [image, setImage] = useState(null);//добавляем картинки как файлы
-    const [progress, setProgress] = useState(0);//анимация загрузки
-    const [caption, setCaption] = useState("");//добаляем подпись
-
-    const handleChange = (event) => {
-        if (event.target.files[0]) {
-            setImage(event.target.files[0]);
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
         }
-    }
+    };
 
     const handleUpload = () => {
         const uploadTask = storage.ref(`images/${image.name}`).put(image);
@@ -21,11 +22,12 @@ function ImageUpload({ username }) {
             "state_changed",
             (snapshot) => {
                 const progress = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
                 setProgress(progress);
             },
             (error) => {
-                console.log(error);
+                // console.log(error);
                 alert(error.message);
             },
             () => {
@@ -33,13 +35,14 @@ function ImageUpload({ username }) {
                     .ref("images")
                     .child(image.name)
                     .getDownloadURL()
-                    .then(url => {
+                    .then((url) => {
                         db.collection("posts").add({
                             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
                             caption: caption,
                             imageUrl: url,
-                            username: username
+                            username: username,
                         });
+
                         setProgress(0);
                         setCaption("");
                         setImage(null);
@@ -49,13 +52,18 @@ function ImageUpload({ username }) {
     };
 
     return (
-        <div>
-            <progress value={progress} max="100" />
-            <input type="text" onChange={(event) => setCaption(event.target.value)} placeholder="Enter the caption..." valuer={caption}></input>
+        <div className="imageUpload">
+            <progress className="imageUpload__progress" value={progress} max="100" />
+            <input
+                type="text"
+                placeholder="Enter a caption..."
+                onChange={event => setCaption(event.target.value)}
+                value={caption} />
+
             <input type="file" onChange={handleChange} />
             <Button onClick={handleUpload}>Upload</Button>
         </div>
     )
-}
+};
 
 export default ImageUpload;
