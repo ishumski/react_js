45de5c8230b37fa31b7589f21569fdeca9e0fa36
ishumski <whalterwhite23@gmail.com>
2021-04-05 14:45:26 +0300

    Add widgetsbar

--- clones_on_react/twitter-clone/src/components/Widgets/Widgets.js
+++ clones_on_react/twitter-clone/src/components/Widgets/Widgets.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Widgets.css";
+import SearchIcon from '@material-ui/icons/Search';
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from "react-twitter-embed";
 
 function Widgets() {
     return (
         <div className="widgets">
-            <h2>WIDGETS</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input type="text" placeholder="Search..." />
+            </div>
+            <div className="widgets__widgetContainer">
+                <TwitterTweetEmbed tweetId={"841418541026877441"} />
+                <TwitterTimelineEmbed
+                    sourceType="profile"
+                    screenName="BBCWorld"
+                    options={{ height: 400 }}
+
+                />
+                <TwitterShareButton url="https://twitter.com/" />
+
+            </div>
+
         </div>
     )
 }
